2d34ee3b00793bea9cd06e77425acf4dfb634e36
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-14 23:21:00 +0900

    feat: Apply Route Config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { RouteConfig } from '@nestjs/platform-fastify';
+import { FastifyRequest } from 'fastify';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +10,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // Code in docs is incorrect
+  // https://docs.nestjs.com/techniques/performance#route-config
+  @RouteConfig({ output: 'hello world' })
+  @Get('/route-config')
+  index(@Req() req: FastifyRequest) {
+    console.log('req.routeOptions.config', req.routeOptions.config); // { output: 'hello world', url: '/route-config', method: 'GET' }
+    // But req.routeOptions.config.output is throw error (Property 'output' does not exist on type 'FastifyContextConfig & FastifyRouteConfig'.)
+    const config = (req.routeOptions.config as { output?: string }) || {};
+    return config.output;
   }
 }
