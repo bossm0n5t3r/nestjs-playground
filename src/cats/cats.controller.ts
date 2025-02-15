31ab4c4017f7491189d5f034207c0a4ff411afa1
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-15 20:10:10 +0900

    feat: Update CatsController
    - Add FastifyRequest to findAll

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
+  findAll(@Req() request: FastifyRequest): string {
+    console.log('request', request);
     return 'This action returns all cats';
   }
 }
