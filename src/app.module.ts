32f7bfa1c3650fb8efbb14a6cfa09b9604f1524b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-16 16:00:50 +0900

    feat: Update AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
