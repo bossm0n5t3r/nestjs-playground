0dc0a6dc05ef24738b0f69261dc6add25271dc20
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-17 22:20:40 +0900

    feat: Update CatsService

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 
 @Injectable()
@@ -9,6 +10,22 @@ export class CatsService {
   create(cat: CreateCatDto) {
     const id = this.cats.length + 1;
     this.cats.push({ id, ...cat });
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    const cat = this.findOne(id);
+    if (!cat) {
+      return;
+    }
+    if (updateCatDto.name) {
+      cat.name = updateCatDto.name;
+    }
+    if (updateCatDto.age) {
+      cat.age = updateCatDto.age;
+    }
+    if (updateCatDto.breed) {
+      cat.breed = updateCatDto.breed;
+    }
   }
 
   findAll(): Cat[] {
