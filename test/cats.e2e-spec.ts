d2bf288a3b3ebe1bad4eb57e2a5624da0a5da980
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-22 17:00:30 +0900

    feat: Update cats.e2e-spec.ts

--- test/cats.e2e-spec.ts
+++ test/cats.e2e-spec.ts
@@ -1,15 +1,27 @@
+import { getModelToken } from '@nestjs/mongoose';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CatsModule } from '../src/cats/cats.module';
+import { CatsController } from '../src/cats/cats.controller';
+import { CatsService } from '../src/cats/cats.service';
+import { Cat } from '../src/cats/schemas/cat.schema';
+
+class MockCatModel {
+  static find = jest.fn().mockReturnThis();
+  static exec = jest.fn().mockResolvedValue([]);
+}
 
 describe('Cats', () => {
   let app: NestFastifyApplication;
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CatsModule],
+      controllers: [CatsController],
+      providers: [
+        CatsService,
+        { provide: getModelToken(Cat.name), useValue: MockCatModel },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication<NestFastifyApplication>(
