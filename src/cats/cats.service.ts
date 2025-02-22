be59e92db4813d3832a8fc20ccd3faf5f11d7a0c
bossm0n5t3r <bossm0n5t3r@gmail.com>
2025-02-22 16:55:30 +0900

    feat: Update CatsService

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,42 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { from, map, Observable, switchMap } from 'rxjs';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
-import { Cat } from './interfaces/cat.interface';
+import { Cat } from './schemas/cat.schema';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cat[] = [];
+  constructor(@InjectModel('Cat') private catModel: Model<Cat>) {}
 
-  create(cat: CreateCatDto) {
-    const id = this.cats.length + 1;
-    this.cats.push({ id, ...cat });
+  create(cat: CreateCatDto): Observable<Cat> {
+    const createdCat = new this.catModel(cat);
+    return from(createdCat.save());
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
-    const cat = this.findOne(id);
-    if (!cat) {
-      return;
-    }
-    if (updateCatDto.name) {
-      cat.name = updateCatDto.name;
-    }
-    if (updateCatDto.age) {
-      cat.age = updateCatDto.age;
-    }
-    if (updateCatDto.breed) {
-      cat.breed = updateCatDto.breed;
-    }
+  update(name: string, updateCatDto: UpdateCatDto): Observable<Cat> {
+    return from(this.catModel.findOne({ name }).exec()).pipe(
+      map((cat) => {
+        if (!cat) {
+          throw new NotFoundException(`Not found: ${name}`);
+        }
+        return cat;
+      }),
+      switchMap((cat) => {
+        console.log('cat', cat);
+        if (updateCatDto.name !== undefined && updateCatDto.name !== null) {
+          cat.name = updateCatDto.name;
+        }
+        if (updateCatDto.age !== undefined && updateCatDto.age !== null) {
+          cat.age = updateCatDto.age;
+        }
+        if (updateCatDto.breed !== undefined && updateCatDto.breed !== null) {
+          cat.breed = updateCatDto.breed;
+        }
+        return from(cat.save());
+      }),
+    );
   }
 
-  findAll(): Cat[] {
-    return this.cats;
+  findAll(): Observable<Cat[]> {
+    return from(this.catModel.find().exec());
   }
 
-  findOne(id: number): Cat {
-    const cat = this.cats.find((cat) => cat.id === id);
-    if (!cat) {
-      throw new Error(`Cat with id ${id} not found`);
-    }
-    return cat;
+  findOne(name: string): Observable<Cat> {
+    const cat = this.catModel.findOne({ name }).exec() as Promise<Cat>;
+    return from(cat);
   }
 }
